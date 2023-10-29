5c2a21109a51bff67b657d6304fd5a4394fe58cc
takumya yamashita <miruo.myan@gmail.com>
2023-10-29 15:06:44 +0900

    add user form to html

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Typing Square' });
 });
 
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'ToDo App' });
+});
+
 module.exports = router;
