bc2450fb515964bcabef66c34a6eebf8dbadb8fd
takumya yamashita <miruo.myan@gmail.com>
2023-12-12 11:52:18 +0900

    add recording function and modify json

--- public/javascripts/record.js
+++ public/javascripts/record.js
@@ -1,112 +1,4 @@
-// startup
-let phaserGame;
-let phaserGameRecord;
-
-let closeBtn;
-let closeBtnRecord;
-let music;
-let btnList;
-let game;
-let sound;
-let score;
-
-function register_music(){
-    switch(this.dataset.musicNumber){
-        
-        case "1":
-            number = 1;
-            src = 'BKOJ001_91BPM.mp3';
-            note = 'BKOJ001_91BPM.json';
-            time = 75000;
-            bpm = 91;
-            first_ts = 11260;
-            beat = 4;
-            break;
-        case "2":
-            number = 2;
-            src = 'BKOJ002_81BPM.mp3';
-            note = 'BKOJ002_81BPM.json';
-            time = 96000;
-            bpm = 81;
-            first_ts = 6320;
-            beat = 4;
-            break;
-        case "3":
-            number = 3;
-            src = 'BKOJ003_120BPM.mp3';
-            note = 'BKOJ003_120BPM.json';
-            time = 48500;
-            bpm = 120;
-            first_ts = 8500;
-            beat = 3;
-            break;
-        case "4":
-            number = 4;
-            src = 'canon.mp3';
-            note = 'canon.json';
-            time = 87000;
-            bpm = 90;
-            first_ts = 11500;
-            beat = 4;
-            break;
-        default:
-            throw new Error('Not registered music number');
-    }
-    music = new Music(number, src, note, time, bpm, first_ts, beat);
-}
-
-function sleep(waitMsec) {
-    var startMsec = new Date();
-   
-    // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
-    while (new Date() - startMsec < waitMsec);
-  }
-
-function tearDown(){
-    game.destroy(removeCanvas=true);
-}
-
-btnList = document.querySelectorAll(".btn-selector")
-for(let i=0; i<btnList.length; i++) {
-    btnList[i].addEventListener('click', register_music);
-}
-
-closeBtn = document.getElementById("btn-close");
-closeBtn.addEventListener('click', tearDown);
-
-closeBtnRecord = document.getElementById("btn-close-record");
-closeBtnRecord.addEventListener('click', tearDown);
-
-modal = document.getElementById("exampleModal");
-modal.addEventListener("shown.bs.modal", () => {
-    if (phaserGame == null) {
-        phaserGame = new PhaserGame();
-    }
-    phaserGame.init();
-});
-
-modalRecord = document.getElementById("recordModal");
-modalRecord.addEventListener("shown.bs.modal", () => {
-    if (phaserGameRecord == null) {
-        phaserGameRecord = new PhaserGameRecord();
-    }
-    phaserGameRecord.init();
-});
-
-class Music {
-    
-    constructor(number, src, note, time, bpm, first_ts, beat){
-        this.number = number;
-        this.src = src;
-        this.note = note;
-        this.time = time; 
-        this.bpm = bpm;
-        this.first_ts = first_ts;
-        this.beat = beat;
-    }
-}
-
-class PhaserGame {
+class PhaserGameRecord extends PhaserGame{
     init() {
         let config = {
             type: Phaser.AUTO,
@@ -114,10 +6,10 @@ class PhaserGame {
                 mode: Phaser.Scale.FIT,
                 autoCenter: Phaser.Scale.CENTER_BOTH
             },
-            parent: 'modal-body',
+            parent: 'modal-body-record',
             width: 1920,
             height: 1080,
-            scene: MenuScene,
+            scene: MenuSceneRecord,
             physics: {
                 default: 'arcade',
                 arcade: {
@@ -133,68 +25,24 @@ class PhaserGame {
 /**
  * First Scene: html games require user to focus the window (click or key down) before playing audio so we do this
  */
-class MenuScene {
-    create() {
-        this.text = this.add.text(
-            1920 / 2,
-            1080 / 2,
-            "SPACEで開始",
-            { fontFamily: "arial", fontSize: "100px" }
-        );
-        this.text.setOrigin(0.5, 0.5);
-    }
-
+class MenuSceneRecord extends MenuScene {
     update() {
         if (isKeyPressed("Space")) {
-            this.scene.add("main", MainScene);
+            this.scene.add("main", MainSceneRecord);
             this.scene.start("main");
         }
     }
 }
 
-class MainScene {
-    preload() {
-        console.log(music.src);
-        this.load.audio("music", "/musics/" + music.src);
-        this.load.image("Stamp", "/images/nikukyu.png");
-        this.load.image("KeyA", "/images/keyA.png");
-        this.load.image("KeyB", "/images/keyB.png");
-        this.load.image("KeyC", "/images/keyC.png");
-        this.load.image("KeyD", "/images/keyD.png");
-        this.load.image("KeyE", "/images/keyE.png");
-        this.load.image("KeyF", "/images/keyF.png");
-        this.load.image("KeyG", "/images/keyG.png");
-        this.load.image("KeyH", "/images/keyH.png");
-        this.load.image("KeyI", "/images/keyI.png");
-        this.load.image("KeyJ", "/images/keyJ.png");
-        this.load.image("KeyK", "/images/keyK.png");
-        this.load.image("KeyL", "/images/keyL.png");
-        this.load.image("KeyM", "/images/keyM.png");
-        this.load.image("KeyN", "/images/keyN.png");
-        this.load.image("KeyO", "/images/keyO.png");
-        this.load.image("KeyP", "/images/keyP.png");
-        this.load.image("KeyQ", "/images/keyQ.png");
-        this.load.image("KeyR", "/images/keyR.png");
-        this.load.image("KeyS", "/images/keyS.png");
-        this.load.image("KeyT", "/images/keyT.png");
-        this.load.image("KeyU", "/images/keyU.png");
-        this.load.image("KeyV", "/images/keyV.png");
-        this.load.image("KeyW", "/images/keyW.png");
-        this.load.image("KeyX", "/images/keyX.png");
-        this.load.image("KeyY", "/images/keyY.png");
-        this.load.image("KeyZ", "/images/keyZ.png");
-        this.load.image('sky', '/images/sky.png');
-        this.load.image('sax_cat_center', '/images/sax_cat.png');
-        this.load.image('sax_cat_left', '/images/sax_cat_left.png');
-        this.load.image('sax_cat_right', '/images/sax_cat_right.png');
-        }
-
+class MainSceneRecord extends MainScene {
     async create() {
         // Notes timestamps, made with the other script "record.html". They are relative to the start of the song, meaning a value of 1000 equals to 1 second after the song has started
         this.notesTimestamps = this.getNote();
         this.timeToFall = 2000; // ms, time for the note to go to the bottom. The lower the faster/hardest
         this.lastNoteIndex = 0; // last note spawned
         this.notes = [];        // array of notes already spawned
+        this.notesRecord = [];  // timestamp array of notes already spawned
+        this.record = [];
         this.colliders = [];    // colliders for player input vs falling note
         this.characters = ["KeyA", "KeyB", "KeyC", "KeyD", "KeyE", "KeyF", "KeyG", "KeyH", "KeyI", "KeyJ", "KeyK", "KeyL", "KeyM", "KeyN", "KeyO", "KeyP", "KeyQ", "KeyR", "KeyS", "KeyT", "KeyU", "KeyV", "KeyW", "KeyX", "KeyY", "KeyZ"];
         this.charIndex = 0;
@@ -216,28 +64,18 @@ class MainScene {
         this.scoreText = this.add.text(100, 100, "SCORE", { fontFamily: "arial", fontSize: "100px" });
         score = 0;
 
-        this.scene.add("result", ResultScene);
+        this.scene.add("result", ResultSceneRecord);
         // We create the audio object and play it
         sound = this.sound.add("music");
         sound.volume = 0.1;
         await sound.play();
         this.startTime = Math.floor(sound.seek * 1000)
-        console.log(sound.seek)
-        console.log(this.startTime)
         console.log("start")
         
     }
-
-    update() {
-        this.handlePlayerInput();
-        this.spawnNotes();
-        this.checkNoteCollisions();
-        this.backgroundAnimation();
-        this.checkFinished();
-    }
-
     spawnNotes() {
         // lets look up to the 10 next notes and spawn if needed
+        //console.log(Math.floor(sound.seek * 1000))
         for (let i = this.lastNoteIndex; i < this.lastNoteIndex + 10; i++) {
             let note = this.notesTimestamps[i];
             if (!note) break;
@@ -247,19 +85,26 @@ class MainScene {
                 note.spawned != true
                 && note.timestamp <=  Math.floor(sound.seek * 1000) - this.startTime + this.timeToFall
             ) {
-                this.spawnNote();
+                this.spawnNote(note.is_main, note.timestamp);
                 this.lastNoteIndex = i;
                 note.spawned = true;
             }
         }
     }
 
-    spawnNote() {
+    spawnNote(is_main, timestamp) {
         // This is self explanatory. Spawn the note and let it fall to the bottom.
         //let note = this.add.circle(1920 / 2, 0, 20, 0xffff00);
-        this.charIndex = Math.floor(Math.random() * this.characters.length);
-        let note = this.add.image(1920, 800, this.characters[this.charIndex]).setDisplaySize(150,150);
+        this.charIndex = 9;
+        let note;
+        if (is_main == 1){
+            note = this.add.image(1920, 800, this.characters[this.charIndex]).setDisplaySize(150,150);
+        }
+        else {
+            note = this.add.image(1920, 800, this.characters[this.charIndex]).setDisplaySize(100,100);
+        }
         this.notes.push(note);
+        this.notesRecord.push(timestamp);
         this.physics.add.existing(note);
         this.physics.moveTo(note, 200, 800, null, this.timeToFall);
     }
@@ -268,7 +113,7 @@ class MainScene {
         this.firstKeysPressed = getFirstKeysPressed()
         if (this.firstKeysPressed.length != 0) {
             // we create a new collider at the position of the red bar
-            let collider = this.add.image(200, 800, "Stamp").setDisplaySize(150,150);
+            let collider = this.add.image(200, 800, "Stamp").setDisplaySize(10,10);
 
             // attach physics
             this.physics.add.existing(collider);
@@ -302,14 +147,18 @@ class MainScene {
             if(this.firstKeyPressed.code==note.texture.key){
                 // the collider collided
                 console.log("success!")
-                console.log(sound.seek)
                 collider.collided = true;
 
                  // remove the collider from list
                  this.colliders.splice(this.colliders.indexOf(collider), 1);
 
                  // destroy the note and remove from list
+                 let t = {}
+                 console.log(this.notesRecord[this.notes.indexOf(note)]);
+                 t.timestamp = this.notesRecord[this.notes.indexOf(note)];
+                 this.record.push(t);
                  note.destroy();
+                 this.notesRecord.splice(this.notes.indexOf(note), 1);
                  this.notes.splice(this.notes.indexOf(note), 1);
 
                  // increase the score and update the text
@@ -319,49 +168,39 @@ class MainScene {
         });
     }
 
-    updateScoreText() {
-        this.scoreText.text = score;
-    }
-
-    backgroundAnimation() {
-        switch (Math.floor(( Math.floor(sound.seek * 1000) - this.startTime) / 1000 )%4){
-            case 0:
-                this.sax_cat.destroy();
-                this.sax_cat = this.add.image(1920 / 2, 400, 'sax_cat_center');
-                break;
-            case 1:
-                this.sax_cat.destroy();
-                this.sax_cat = this.add.image(1920 / 2, 400, 'sax_cat_left');
-                break;
-            case 2:
-                this.sax_cat.destroy();
-                this.sax_cat = this.add.image(1920 / 2, 400, 'sax_cat_center');
-                break;
-            case 3:
-                this.sax_cat.destroy();
-                this.sax_cat = this.add.image(1920 / 2, 400, 'sax_cat_right');
-                break;
-
-        }
-    }
-    
-
     checkFinished() {
         
         if ( Math.floor(sound.seek * 1000) - this.startTime >= this.endTimestamp) {
+            console.log(this.record);
             this.scene.start("result");
         }
     }
 
     getNote() {
-        var request = new XMLHttpRequest();
-        request.open("GET", "/jsons/" + music.note, false);
-        request.send(null);
-        return JSON.parse(request.responseText);
+        let timestamps = [];
+        let span = 1000 * 60 / music.bpm / music.beat;
+        let timestamp = {};
+        let i = 0;
+        while(1) {       
+            let t = {};
+            timestamp = Math.floor(music.first_ts + span * i);
+            if (timestamp >= music.time)
+                break;
+            t.timestamp = timestamp;
+            if (i % music.beat == 0) {
+                t.is_main = 1
+            }
+            else {
+                t.is_main = 0
+            }
+            timestamps.push(t);
+            i += 1;
+        }
+        return timestamps;
     }
 }
 
-class ResultScene {
+class ResultSceneRecord extends ResultScene {
     create() {
         this.text = this.add.text(
             1920 / 2,
@@ -408,7 +247,7 @@ class ResultScene {
             xmlHttpRequest.setRequestHeader("content-type", "application/json");
             xmlHttpRequest.send(JSON.stringify(data));
 
-            closeBtn.click();
+            closeBtnRecord.click();
         }
     }
 }
